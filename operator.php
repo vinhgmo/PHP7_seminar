d0f23603461a159572d4e5ac28895c20b15f55a0
vinhkq <vinhkq@runsystem.net>
2015-12-02 18:37:49 +0700

    Update code

--- operator.php
+++ operator.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Seminar</title>
+    <title>PHP7 Operator</title>
 </head>
 <body>
     <h3>Combined Comparison Operator <=> </h3>
@@ -10,17 +10,23 @@
     <h5>So sánh 2 <=> 1: </h5> <?php var_dump(2<=>1); ?>
     <h5>So sánh 1 <=> 1: </h5> <?php var_dump(1<=>1); ?>
 
+    <hr>
+
     <h3>Null Coalesce Operator ??</h3>
-    <h5>Pre PHP 7: </h5>
+    <h5>Pre PHP 7: isset($a) ? $a : 'index';</h5>
     <?php
     $a = isset($a) ? $a : 'index';
     var_dump($a);
      ?>
-    <h5>PHP 7: </h5>
+    <h5>PHP 7: isset($b) ?? 'index';</h5>
     <?php
-    $b = 1;
     $b = isset($b) ?? 'index';
     var_dump($b);
      ?>
+
+    <hr>
+
+
+
 </body>
 </html>
